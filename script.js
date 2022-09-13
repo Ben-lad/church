23fa6b3758d70c0e584a90dd5ef035e74598eb25
benjaminladegbayeademide <112470050+benjaminladegbayeademide@users.noreply.github.com>
2022-09-13 10:35:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+
+
+(() =>{
+ 
+    const openNavMenu = document.querySelector(".open-nav-menu"),
+    closeNavMenu = document.querySelector(".close-nav-menu"),
+    navMenu = document.querySelector(".nav-menu"),
+    menuOverlay = document.querySelector(".menu-overlay"),
+    mediaSize = 991;
+  
+    openNavMenu.addEventListener("click", toggleNav);
+    closeNavMenu.addEventListener("click", toggleNav);
+    // close the navMenu by clicking outside
+    menuOverlay.addEventListener("click", toggleNav);
+  
+    function toggleNav() {
+        navMenu.classList.toggle("open");
+        menuOverlay.classList.toggle("active");
+        document.body.classList.toggle("hidden-scrolling");
+    }
+  
+    navMenu.addEventListener("click", (event) =>{
+        if(event.target.hasAttribute("data-toggle") && 
+            window.innerWidth <= mediaSize){
+            // prevent default anchor click behavior
+            event.preventDefault();
+            const menuItemHasChildren = event.target.parentElement;
+          // if menuItemHasChildren is already expanded, collapse it
+          if(menuItemHasChildren.classList.contains("active")){
+              collapseSubMenu();
+          }
+          else{
+            // collapse existing expanded menuItemHasChildren
+            if(navMenu.querySelector(".menu-item-has-children.active")){
+              collapseSubMenu();
+            }
+            // expand new menuItemHasChildren
+            menuItemHasChildren.classList.add("active");
+            const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+            subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+          }
+        }
+    });
+    function collapseSubMenu(){
+        navMenu.querySelector(".menu-item-has-children.active .sub-menu")
+        .removeAttribute("style");
+        navMenu.querySelector(".menu-item-has-children.active")
+        .classList.remove("active");
+    }
+    function resizeFix(){
+         // if navMenu is open ,close it
+         if(navMenu.classList.contains("open")){
+             toggleNav();
+         }
+         // if menuItemHasChildren is expanded , collapse it
+         if(navMenu.querySelector(".menu-item-has-children.active")){
+              collapseSubMenu();
+       }
+    }
+  
+    window.addEventListener("resize", function(){
+       if(this.innerWidth > mediaSize){
+           resizeFix();
+       }
+    });
+  
+  })();
+  // the typing
+  const type = new Typed(".typing",{
+    strings: ["", "with better satisfaction", "with better understanding","with over thousands of people", "with us and become your own master", "with us on our youtube channels","with us and pratice with us personal", "what are you waiting for ?","enroll now"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+   });
+  //  the scroll
+   window.addEventListener("scroll", function(e){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky" , window.scrollY > 0)
+   })
+
+   window.addEventListener("scroll", function(e){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky" , window.scrollY > 0)
+   })
+   var scriptUrl = 'https:\/\/www.youtube.com\/s\/player\/f96f6702\/www-widgetapi.vflset\/www-widgetapi.js';try{var ttPolicy=window.trustedTypes.createPolicy("youtube-widget-api",{createScriptURL:function(x){return x}});scriptUrl=ttPolicy.createScriptURL(scriptUrl)}catch(e){}var YT;if(!window["YT"])YT={loading:0,loaded:0};var YTConfig;if(!window["YTConfig"])YTConfig={"host":"https://www.youtube.com"};
+if(!YT.loading){YT.loading=1;(function(){var l=[];YT.ready=function(f){if(YT.loaded)f();else l.push(f)};window.onYTReady=function(){YT.loaded=1;for(var i=0;i<l.length;i++)try{l[i]()}catch(e$0){}};YT.setConfig=function(c){for(var k in c)if(c.hasOwnProperty(k))YTConfig[k]=c[k]};var a=document.createElement("script");a.type="text/javascript";a.id="www-widgetapi-script";a.src=scriptUrl;a.async=true;var c=document.currentScript;if(c){var n=c.nonce||c.getAttribute("nonce");if(n)a.setAttribute("nonce",n)}var b=
+document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})()};
+   
+  
